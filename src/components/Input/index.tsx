2916b006142c3a321e9b5933dabb263a48a2714e
GyasminAssunção <gyasminassuncao@uni9.edu.br>
2020-06-09 20:26:59 -0300

    Dando focus aos inputs

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,12 @@
 /* eslint-disable react/prop-types */
-import React, { useRef, useEffect, useImperativeHandle, forwardRef } from 'react';
+import React, {
+  useRef,
+  useEffect,
+  useImperativeHandle,
+  forwardRef,
+  useState,
+  useCallback,
+} from 'react';
 import { useField } from '@unform/core';
 import { TextInputProps } from 'react-native';
 import { Container, TextInput, Icon } from './styles';
@@ -17,13 +24,25 @@ interface InputRef {
   focus(): void;
 }
 
-const Input: React.RefForwardingComponent< InputRef, InputProps> = (
+const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   { name, icon, ...rest },
   ref,
 ) => {
   const inputElementRef = useRef<any>(null);
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  },[])
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputValueRef.current.value);
+  },[])
 
   useImperativeHandle(ref, () => ({
     focus(){
@@ -48,12 +67,18 @@ const Input: React.RefForwardingComponent< InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
       <TextInput
         ref={inputElementRef}
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={value => {
           inputValueRef.current.value = value;
         }}
